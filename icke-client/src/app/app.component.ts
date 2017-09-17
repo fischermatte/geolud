66e4dc4fc6f1ef2247aa709ee08a953d561bbf1f
fischermatte <fischermatte@posteo.net>
2017-09-18 00:25:59 +0200

    improved navbar

--- icke-client/src/app/app.component.ts
+++ icke-client/src/app/app.component.ts
@@ -6,5 +6,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public isCollapsed: boolean = false;
+  public isCollapsed: boolean = true;
 }
