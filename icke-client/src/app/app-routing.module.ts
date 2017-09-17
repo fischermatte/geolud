44179b7761a8986b3346f76638d773aca5fa5eb6
fischermatte <fischermatte@posteo.net>
2017-09-17 22:51:10 +0200

    added some routes

--- icke-client/src/app/app-routing.module.ts
+++ icke-client/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {SkillsComponent} from "./skills/skills.component";
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'skills', component: SkillsComponent},
 ];
 
 @NgModule({
