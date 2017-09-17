056ecffafb024ebcd2e6cbe4f641d6db2495ce21
fischermatte <fischermatte@posteo.net>
2017-09-18 00:14:05 +0200

    started with responsive navbar (not yet working with ngx-bootstrap)

--- icke-client/src/app/app.module.ts
+++ icke-client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { CollapseModule } from 'ngx-bootstrap';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -20,7 +20,7 @@ import {HomeComponent} from './home/home.component';
     HomeComponent
   ],
   imports: [
-    NgbModule.forRoot(),
+    CollapseModule.forRoot(),
     BrowserModule,
     AppRoutingModule
   ],
