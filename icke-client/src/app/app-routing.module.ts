3864da0a2e54c80cd249b4b9e803e22b24a9d2f3
fischermatte <fischermatte@posteo.net>
2017-09-17 23:11:28 +0200

    added all states

--- icke-client/src/app/app-routing.module.ts
+++ icke-client/src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {SkillsComponent} from "./skills/skills.component";
+import {ResumeComponent} from "./resume/resume.component";
+import {ProjectsComponent} from "./projects/projects.component";
+import {ContactComponent} from "./contact/contact.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
+  {path: 'resume', component: ResumeComponent},
   {path: 'skills', component: SkillsComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'contact', component: ContactComponent}
 ];
 
 @NgModule({
