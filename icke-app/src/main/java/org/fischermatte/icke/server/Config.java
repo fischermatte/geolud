a373c64cdb16ad221c8d87fbb4c5af9652bf338e
fischermatte <fischermatte@posteo.net>
2017-09-23 23:16:49 +0200

    html 5 routes are now supported

--- icke-app/src/main/java/org/fischermatte/icke/server/Config.java
+++ icke-app/src/main/java/org/fischermatte/icke/server/Config.java
@@ -4,12 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 
+import java.util.List;
+
+import static java.util.Arrays.asList;
 import static org.springframework.web.reactive.function.server.RouterFunctions.resources;
 
 @Configuration
 public class Config {
+
+    private static final List<String> APP_STATES = asList("/home", "/skills", "/resume", "/projects", "/contact");
 
     @Bean
     public RouterFunction<?> router() {
@@ -20,12 +26,22 @@ public class Config {
     @Bean
     public WebFilter redirectToIndex() {
         return (exchange, chain) -> {
-            if (exchange.getRequest().getURI().getPath().equals("/")) {
-                return chain.filter(exchange.mutate().request(exchange.getRequest().mutate().path("/index.html")
-                        .build()).build());
+            if (isBaseUrl(exchange) || isAppState(exchange)) {
+                return chain.filter(exchange.mutate().request(
+                        exchange.getRequest().mutate().path("/index.html").build()).build());
             }
             return chain.filter(exchange);
         };
+    }
+
+    private boolean isBaseUrl(ServerWebExchange exchange) {
+        return exchange.getRequest().getURI().getPath().equals("/");
+    }
+
+
+    // FIXME make html 5 routing more generic
+    private boolean isAppState(ServerWebExchange exchange) {
+        return APP_STATES.stream().anyMatch(exchange.getRequest().getURI().getPath()::startsWith);
     }
 
 }
