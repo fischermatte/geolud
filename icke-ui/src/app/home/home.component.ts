daea2783ac260f8ad99533618a5f3f7a0efec8cb
fischermatte <fischermatte@posteo.net>
2017-09-30 22:24:03 +0200

    tried scrolling

--- icke-ui/src/app/home/home.component.ts
+++ icke-ui/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -7,10 +8,16 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() {
+  constructor(private route: ActivatedRoute) {
   }
 
   ngOnInit() {
+    this.route.fragment.subscribe(f => {
+      const element = document.querySelector("#" + f);
+      if (element) {
+        element.scrollIntoView(element);
+      }
+    })
   }
 
 }
