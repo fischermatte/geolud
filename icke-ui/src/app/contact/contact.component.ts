716583e96a4bc7bbf87cdcd82a047f4b21dfde7e
fischermatte <fischermatte@posteo.net>
2017-10-03 23:10:01 +0200

    icke-2 add feedback for contact request

--- icke-ui/src/app/contact/contact.component.ts
+++ icke-ui/src/app/contact/contact.component.ts
@@ -10,6 +10,7 @@ import {ContactRequest} from "../../../../icke-api/target/api/model/ts/contact";
 export class ContactComponent implements OnInit {
 
   contactRequest: ContactRequest;
+  feedback: Alert;
   @ViewChild('contactForm') contactForm: any;
 
   constructor(private http: HttpClient) {
@@ -24,13 +25,31 @@ export class ContactComponent implements OnInit {
       // note we set the response type to text due to this issue:  https://github.com/angular/angular/issues/18680
       this.http.post("/api/contact", this.contactRequest, {responseType: 'text'}).subscribe(
         response => {
+          this.feedback = <Alert>{
+            type: "success",
+            message: "Contact request was submitted!"
+          };
           this.contactForm.reset();
         },
         error => {
-          console.log("Error occured" + error);
+          this.feedback = <Alert>{
+            type: "danger",
+            message: "Failed to submit contact request due to some very mysterious reasons !"
+          };
         }
       );
     }
   }
 
+  public closeAlert() {
+    this.feedback = null;
+  }
+
+
 }
+
+class Alert {
+  type: string;
+  message: string;
+}
+
