07f0ce64acd20f64e0b6fcec0a4a020d587a0127
fischermatte <fischermatte@posteo.net>
2017-10-10 17:22:16 +0200

    added more test data

--- icke-server/src/main/java/io/fischermatte/icke/server/bootstrap/DataInitializer.java
+++ icke-server/src/main/java/io/fischermatte/icke/server/bootstrap/DataInitializer.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import static java.util.Arrays.asList;
+
 @Component
 public class DataInitializer {
     private static final Logger LOG = LoggerFactory.getLogger(DataInitializer.class);
@@ -19,18 +21,32 @@ public class DataInitializer {
     }
 
     public void intialize() {
-        projectRepository.save(new Project()
-                .withId(UUID.randomUUID())
-                .withTitle("Live Ticker Tour de France 2001")
-                .withDescription("Flash-based Live Ticker for the Tour de France 2001. Real time stage tracking with maps and profiles.")
-                .withCustomer(new Customer()
-                        .withName("SPIEGEL-ONLINE")
-                        .withUrl("http://www.spiegel.de/"))
-                .withInterval(new Interval()
-                        .withFrom(LocalDate.of(2001, 1, 1))
-                        .withTo(LocalDate.of(2001, 1, 1)))
-                .withLink(new Link().withTitle("screenshot 1").withUrl("assets/projects/tour/tour_screenshot_overview.png"))
-                .withLink(new Link().withTitle("screenshot 2").withUrl("assets/projects/tour/tour_screenshot_etappe.png")));
+        projectRepository.save(asList(
+                new Project()
+                        .withId(UUID.randomUUID())
+                        .withTitle("Online XML/Flash-based map for www.russlandpartner.de")
+                        .withDescription("A Flash-based map with information about Russian-German town twinning. Used technologies: XML, Adobe Flash, ActionScript.")
+                        .withCustomer(new Customer()
+                                .withName("Deutsch-Russische Forum e.V.")
+                                .withUrl("http://www.deutsch-russisches-forum.de/"))
+                        .withInterval(new Interval()
+                                .withFrom(LocalDate.of(2004, 1, 1))
+                                .withTo(LocalDate.of(2004, 1, 1)))
+                        .withLink(new Link().withTitle("screenshot 1").withUrl("assets/projects/twin/screenshot_germany.png"))
+                        .withLink(new Link().withTitle("screenshot 2").withUrl("assets/projects/twin/screenshot_germany.png")),
+                new Project()
+                        .withId(UUID.randomUUID())
+                        .withTitle("Live Ticker Tour de France 2001")
+                        .withDescription("Flash-based Live Ticker for the Tour de France 2001. Real time stage tracking with maps and profiles.")
+                        .withCustomer(new Customer()
+                                .withName("SPIEGEL-ONLINE")
+                                .withUrl("http://www.spiegel.de/"))
+                        .withInterval(new Interval()
+                                .withFrom(LocalDate.of(2001, 1, 1))
+                                .withTo(LocalDate.of(2001, 1, 1)))
+                        .withLink(new Link().withTitle("screenshot 1").withUrl("assets/projects/tour/tour_screenshot_overview.png"))
+                        .withLink(new Link().withTitle("screenshot 2").withUrl("assets/projects/tour/tour_screenshot_etappe.png")))
+        );
     }
 
 }
