a05376f6d49a7225d48d3f67910b64c8448a86b7
fischermatte <fischermatte@posteo.net>
2017-10-12 15:51:42 +0200

    enabled cors support for server

--- icke-server/src/main/java/io/fischermatte/icke/server/ApplicationConfiguration.java
+++ icke-server/src/main/java/io/fischermatte/icke/server/ApplicationConfiguration.java
@@ -10,6 +10,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -32,6 +35,19 @@ public class ApplicationConfiguration {
     public Executor taskExecutor() {
         return new SimpleAsyncTaskExecutor();
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
+
 
     @Bean
     public ProjectRepository projectRepository() {
