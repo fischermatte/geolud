10634af09a112795ea34d4f287e4984de37f2f4d
fischermatte <fischermatte@posteo.net>
2017-12-10 21:24:03 +0100

    #20 scroll to top after route change / added back rounded corners

--- icke-ui/src/app/app.component.ts
+++ icke-ui/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public navbarCollapsed = true;
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((evt) => {
+      if (evt instanceof NavigationEnd) {
+        window.scrollTo(0, 0);
+      }
+    });
+  }
 }
