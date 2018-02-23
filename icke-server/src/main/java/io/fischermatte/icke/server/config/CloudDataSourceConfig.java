a4f85e0b4a575a255d7dc1cc2ed7b93fe894b2c5
fischermatte <fischermatte@posteo.net>
2018-02-24 00:34:51 +0100

    changed cloud datasource lookup strategy

--- icke-server/src/main/java/io/fischermatte/icke/server/config/CloudDataSourceConfig.java
+++ icke-server/src/main/java/io/fischermatte/icke/server/config/CloudDataSourceConfig.java
@@ -1,8 +1,9 @@
 package io.fischermatte.icke.server.config;
 
-import org.springframework.cloud.config.java.AbstractCloudConfig;
-import org.springframework.cloud.service.PooledServiceConnectorConfig.PoolConfig;
-import org.springframework.cloud.service.relational.DataSourceConfig;
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.service.common.PostgresqlServiceInfo;
+import org.springframework.cloud.util.StandardUriInfoFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,14 +12,25 @@ import javax.sql.DataSource;
 
 @Configuration
 @Profile({"cloud"})
-public class CloudDataSourceConfig extends AbstractCloudConfig {
+public class CloudDataSourceConfig {
+
+    private StandardUriInfoFactory uriInfoFactory = new StandardUriInfoFactory();
 
     @Bean
-    public DataSource dataSource() {
-        // FAB put in for pooling (min=5,max=30,wait=3000)
-        PoolConfig poolConfig = new PoolConfig(5, 30, 3000);
-        DataSourceConfig dataSourceConfig = new DataSourceConfig(poolConfig, null);
-        return connectionFactory().dataSource(dataSourceConfig);
+    public DataSource dataSource(@Value("${application.cloud.datasource.uri}") String uri) {
+        // Note: lookup via connectionFactory().dataSource(); did not work with elephant sql on ibm bluemix
+        PostgresqlServiceInfo serviceInfo = new PostgresqlServiceInfo("geolud-db", uri);
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(serviceInfo.getJdbcUrl() );
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUsername(serviceInfo.getUserName());
+        dataSource.setPassword(serviceInfo.getPassword());
+        return dataSource;
+
     }
+//
+//    public static void main (String[] args) {
+//        PostgresqlServiceInfo serviceInfo = new PostgresqlServiceInfo("geolud-db", "postgres://kigxjalj:hR5uH3L6I7FPTlzfoA3vl9SKnFjEN74n@hanno-01.db.elephantsql.com:5432/kigxjalj");
+//    }
 
 }
