13419d56e50caffc067c10c233ca27d0e319bf6b
fischermatte <fischermatte@posteo.net>
2018-02-23 22:37:47 +0100

    added integration test for jpa querydsl specifications

--- icke-server/src/test/java/io/fischermatte/icke/server/repository/ProjectSpecificationsTest.java
+++ icke-server/src/test/java/io/fischermatte/icke/server/repository/ProjectSpecificationsTest.java
@@ -1,12 +1,17 @@
 package io.fischermatte.icke.server.repository;
 
+import io.fischermatte.icke.server.domain.project.Interval;
+import io.fischermatte.icke.server.domain.project.Project;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @ExtendWith(SpringExtension.class)
@@ -18,6 +23,26 @@ class ProjectSpecificationsTest {
 
     @Test
     void titleAndIntervalAreEqual() {
-        assertNotNull(projectRepository);
+        LocalDate startDate = LocalDate.of(2018, 1, 1);
+        Project project1 = projectRepository.save(new Project()
+                .withTitle("Title 1")
+                .withInterval(new Interval()
+                        .withFrom(startDate)
+                        .withTo(startDate.plusDays(1))));
+        Project project2 = projectRepository.save(new Project()
+                .withTitle("Title 2")
+                .withInterval(new Interval()
+                        .withFrom(startDate)
+                        .withTo(startDate.plusDays(1))));
+        Project project3 = projectRepository.save(new Project()
+                .withTitle("Title 2")
+                .withInterval(new Interval()
+                        .withFrom(startDate.plusDays(2))
+                        .withTo(startDate.plusDays(3))));
+
+        assertTrue(projectRepository.exists(ProjectSpecifications.titleAndIntervalAreEqual(project1.getTitle(), project1.getInterval())));
+        assertTrue(projectRepository.exists(ProjectSpecifications.titleAndIntervalAreEqual(project2.getTitle(), project2.getInterval())));
+        assertTrue(projectRepository.exists(ProjectSpecifications.titleAndIntervalAreEqual(project3.getTitle(), project3.getInterval())));
+        assertFalse(projectRepository.exists(ProjectSpecifications.titleAndIntervalAreEqual("Title 1", new Interval().withFrom(startDate).withTo(startDate))));
     }
 }
