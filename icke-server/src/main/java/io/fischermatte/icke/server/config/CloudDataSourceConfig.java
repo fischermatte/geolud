3b047fe29bfc918128d37974d036eae1edc862e6
fischermatte <fischermatte@posteo.net>
2018-02-24 01:04:50 +0100

    added cf spring connector dependency and limitated connection pool

--- icke-server/src/main/java/io/fischermatte/icke/server/config/CloudDataSourceConfig.java
+++ icke-server/src/main/java/io/fischermatte/icke/server/config/CloudDataSourceConfig.java
@@ -1,9 +1,8 @@
 package io.fischermatte.icke.server.config;
 
-import org.apache.commons.dbcp.BasicDataSource;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.service.common.PostgresqlServiceInfo;
-import org.springframework.cloud.util.StandardUriInfoFactory;
+import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.cloud.service.PooledServiceConnectorConfig;
+import org.springframework.cloud.service.relational.DataSourceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -12,25 +11,27 @@ import javax.sql.DataSource;
 
 @Configuration
 @Profile({"cloud"})
-public class CloudDataSourceConfig {
-
-    private StandardUriInfoFactory uriInfoFactory = new StandardUriInfoFactory();
+public class CloudDataSourceConfig extends AbstractCloudConfig {
 
     @Bean
-    public DataSource dataSource(@Value("${application.cloud.datasource.uri}") String uri) {
-        // Note: lookup via connectionFactory().dataSource(); did not work with elephant sql on ibm bluemix
-        PostgresqlServiceInfo serviceInfo = new PostgresqlServiceInfo("geolud-db", uri);
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setUrl(serviceInfo.getJdbcUrl() );
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUsername(serviceInfo.getUserName());
-        dataSource.setPassword(serviceInfo.getPassword());
-        return dataSource;
-
+    public DataSource dataSource() {
+        // FAB put in for pooling (min=5,max=30,wait=3000)
+        PooledServiceConnectorConfig.PoolConfig poolConfig = new PooledServiceConnectorConfig.PoolConfig(1, 5, 3000);
+        DataSourceConfig dataSourceConfig = new DataSourceConfig(poolConfig, null);
+        return connectionFactory().dataSource(dataSourceConfig);
     }
+
+//    @Bean
+//    public DataSource dataSource(@Value("${application.cloud.datasource.uri}") String uri) {
+//        // Note: lookup via connectionFactory().dataSource(); did not work with elephant sql on ibm bluemix
+//        PostgresqlServiceInfo serviceInfo = new PostgresqlServiceInfo("geolud-db", uri);
+//        BasicDataSource dataSource = new BasicDataSource();
+//        dataSource.setUrl(serviceInfo.getJdbcUrl() );
+//        dataSource.setDriverClassName("org.postgresql.Driver");
+//        dataSource.setUsername(serviceInfo.getUserName());
+//        dataSource.setPassword(serviceInfo.getPassword());
+//        return dataSource;
 //
-//    public static void main (String[] args) {
-//        PostgresqlServiceInfo serviceInfo = new PostgresqlServiceInfo("geolud-db", "postgres://kigxjalj:hR5uH3L6I7FPTlzfoA3vl9SKnFjEN74n@hanno-01.db.elephantsql.com:5432/kigxjalj");
 //    }
 
 }
