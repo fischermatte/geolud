fbe73caadf0d93406891822d4413de82abd23682
fischermatte <fischermatte@posteo.net>
2018-02-26 23:29:54 +0100

    fixed case in mysql sql script

--- icke-server/src/main/resources/db/migration/mysql/V1__init.sql
+++ icke-server/src/main/resources/db/migration/mysql/V1__init.sql
@@ -1,4 +1,5 @@
-CREATE TABLE LINK (PROJECT_ID CHAR(40) NOT NULL, TITLE VARCHAR(1024), URL VARCHAR(1024)) ENGINE=MYISAM;
-CREATE TABLE PROJECT (ID CHAR(40) NOT NULL, CUSTOMER_NAME VARCHAR(255), CUSTOMER_URL VARCHAR(255), DESCRIPTION LONGTEXT, INTERVAL_FROM DATE, INTERVAL_TO DATE, TITLE VARCHAR(500), PRIMARY KEY (ID)) ENGINE=MYISAM;
-ALTER TABLE PROJECT ADD CONSTRAINT UNIQUE_TITLE_INTERVAL UNIQUE (TITLE, INTERVAL_FROM, INTERVAL_TO);
-ALTER TABLE LINK ADD CONSTRAINT FK_PROJECT_ID FOREIGN KEY (PROJECT_ID) REFERENCES PROJECT (ID);
+create table link (project_id varchar(40) not null, title varchar(1024), url varchar(1024)) ENGINE=INNODB CHARACTER SET utf8 COLLATE utf8_bin;
+create table project (id varchar(40) not null, customer_name varchar(255), customer_url varchar(255), description longtext, interval_from date, interval_to date, title varchar(500), primary key (id)) ENGINE=INNODB CHARACTER SET utf8 COLLATE utf8_bin;
+alter table project add constraint UNIQUE_TITLE_INTERVAL unique (title, interval_from, interval_to);
+alter table link add constraint FK_PROJECT_ID foreign key (project_id) references project (id);
+
