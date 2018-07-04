1cf41cd54e5b82aadc011f599f32736ab4e2d549
fischermatte <fischermatte@posteo.net>
2018-07-04 16:24:21 +0200

    minor cleanup

--- geolud-client/zip-build.js
+++ geolud-client/zip-build.js
@@ -2,26 +2,19 @@ const version = require('./package.json').version;
 const fs = require('fs');
 const archiver = require('archiver');
 
-if (!fs.existsSync('target')){
+if (!fs.existsSync('target')) {
   fs.mkdirSync('target');
 }
 
-const output = fs.createWriteStream('target/geolud-client-'+ version + '.zip');
+const output = fs.createWriteStream('target/geolud-client-' + version + '.zip');
 const archive = archiver('zip');
-
-output.on('close', function () {
-  console.log(archive.pointer() + ' total bytes');
-  console.log('archiver has been finalized and the output file descriptor has closed.');
-});
 
 archive.on('error', function (err) {
   throw err;
 });
 
 
-
-
-  archive.pipe(output);
+archive.pipe(output);
 archive.glob('**/*', {
   cwd: 'dist',
   ignore: ['node_modules/**'],
