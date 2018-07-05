463b8dfb407712609fde0cfd13657239275c86c6
fischermatte <fischermatte@posteo.net>
2018-07-05 22:21:10 +0200

    fixed e2e test

--- geolud-client/e2e/app.e2e-spec.ts
+++ geolud-client/e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('geolud-client App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toContain('CODING');
   });
 });
