86f0b8712c7c77eecae742825f185ce41b4686ab
fischermatte <fischermatte@posteo.net>
2018-07-06 22:34:41 +0200

    ongoing universal integration

--- geolud-client/src/app/app-config.service.ts
+++ geolud-client/src/app/app-config.service.ts
@@ -1,17 +1,22 @@
-import {Injectable} from '@angular/core';
+import {Inject, Injectable, Optional} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../environments/environment';
 import {AppConfig} from './app-config';
+import {APP_BASE_HREF} from '@angular/common';
 
 @Injectable()
 export class AppConfigService {
 
-  constructor(private httpClient: HttpClient) {
+  private url: string;
+
+  constructor(private httpClient: HttpClient,
+              @Optional() @Inject(APP_BASE_HREF) origin: string) {
+    this.url = `${origin}/api/app-config`;
   }
 
 
   loadConfig(): any {
-    return this.httpClient.get('/api/app-config')
+    return this.httpClient.get(this.url)
       .toPromise()
       .then((appConfig: AppConfig) => {
         environment.appConfig = appConfig;
