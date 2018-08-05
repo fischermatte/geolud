8accf88c5583b28ede5cc106e81f8b856c185c4c
fischermatte <fischermatte@posteo.net>
2018-08-05 14:29:33 +0200

    added rxjava adapter

--- geolud-server/src/main/java/io/fischermatte/geolud/server/notification/NotificationService.java
+++ geolud-server/src/main/java/io/fischermatte/geolud/server/notification/NotificationService.java
@@ -2,14 +2,23 @@ package io.fischermatte.geolud.server.notification;
 
 import io.fischermatte.geolud.server.chat.ChatMessage;
 import io.fischermatte.geolud.server.notification.repository.PushSubscriptionRepository;
+import io.reactivex.Completable;
 import nl.martijndwars.webpush.Notification;
 import nl.martijndwars.webpush.PushService;
 import nl.martijndwars.webpush.Subscription;
+import org.apache.http.HttpResponse;
+import org.jose4j.lang.JoseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.concurrent.Future;
+
+import static reactor.adapter.rxjava.RxJava2Adapter.completableToMono;
 
 @Service
 public class NotificationService {
@@ -27,11 +36,16 @@ public class NotificationService {
         return subscriptionRepository.findAll().flatMap(subscription ->
                 sendNotification(subscription, message)
         );
-
     }
 
     private Mono<Void> sendNotification(Subscription subscription, ChatMessage message) {
-        Notification notification = new Notification(subscription, message.getText());
-        return pushService.sendAsync(notification);
+        try {
+            Notification notification = new Notification(subscription, message.getText());
+            Future<HttpResponse> future = pushService.sendAsync(notification);
+            return completableToMono(Completable.fromFuture(future));
+        } catch (GeneralSecurityException | IOException | JoseException e) {
+            LOG.error("failed to send notification",  e);
+            return Mono.empty();
+        }
     }
 }
