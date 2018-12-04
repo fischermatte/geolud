ce068ee9eef44ab0ee9371a9a62f0f1c2588e628
fischermatte <fischermatte@posteo.net>
2018-12-04 17:47:06 +0100

    added contact request

--- geolud-server-node/src/contact/contact.controller.ts
+++ geolud-server-node/src/contact/contact.controller.ts
@@ -1,11 +1,18 @@
-import {Controller, Get, Post} from '@nestjs/common';
+import { Controller, Inject, Post } from '@nestjs/common';
+import { MailService } from '../mail/mail.service';
+
+interface ContactRequest {
+  name?: string;
+  email: string;
+  message: string;
+}
 
 @Controller('contact')
 export class ContactController {
+  constructor(@Inject('MailService') private mailService: MailService) {}
 
-    @Post()
-    submitContactRequest(): void {
-        //
-    }
-
+  @Post()
+  submitContactRequest(@Body() contactRequest: ContactRequest): void {
+    this.mailService.sendEmail(contactRequest.name, contactRequest.email, contactRequest.message);
+  }
 }
