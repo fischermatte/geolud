a99e8f661c0b1ab5b70151d41233e7b5693f68c8
fischermatte <fischermatte@posteo.net>
2018-12-07 21:47:10 +0100

    ongoing websocket integration

--- geolud-server-node/src/api/chat/chat.gateway.ts
+++ geolud-server-node/src/api/chat/chat.gateway.ts
@@ -1,26 +1,27 @@
 import {OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse} from '@nestjs/websockets';
 import { Observable, of, ReplaySubject } from 'rxjs';
 import { ChatMessage } from './chat.model';
-import {ALL_PROJECTS} from '../../initialization/data-initializer.projects';
-import {OnModuleInit} from '@nestjs/common';
+import { Server} from 'ws';
 
 @WebSocketGateway()
 export class ChatGateway implements OnGatewayInit {
-  server;
+  server:Server;
 
   private subject: ReplaySubject<ChatMessage> = new ReplaySubject(10, 1000 * 60 * 2);
 
 
-  afterInit(server: WebSocketServer): void {
+  afterInit(server:Server): void {
     this.server = server;
-    this.subject.subscribe(value => {
-      this.server.emit();
+    this.subject.subscribe(message => {
+      this.server.clients.forEach(client => {
+          client.send(JSON.stringify({ event: 'chat', data: message }));
+      })
     });
   }
   @SubscribeMessage('chat')
   onMessage(client, message: ChatMessage): Observable<WsResponse<ChatMessage>> {
     message.timestamp = new Date();
     this.subject.next(message);
-    return of({ event: 'confirmation', data: message });
+    return of({ event: 'chat', data: message });
   }
 }
