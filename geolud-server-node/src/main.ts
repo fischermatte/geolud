7766223b0b5d0a7324672014f658f1b906e33f46
fischermatte <fischermatte@posteo.net>
2019-02-20 21:01:21 +0100

    updated proxy conf

--- geolud-server-node/src/main.ts
+++ geolud-server-node/src/main.ts
@@ -2,11 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/websockets';
 
+const port = process.env.PORT || 8080;
+
+console.info(`Serving at port ${port}`);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: console,
     cors: true,
   });
-  await app.useWebSocketAdapter(new WsAdapter(app)).listen(8080);
+  await app.useWebSocketAdapter(new WsAdapter(app)).listen(port);
 }
 bootstrap();
