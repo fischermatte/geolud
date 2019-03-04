0a4985d247a2a22fdd198e617338a3e487cb1ea3
fischermatte <fischermatte@posteo.net>
2019-03-05 00:25:04 +0100

    fixing build issue

--- geolud-client/projects/geolud-client/app/core/environment.initializer.ts
+++ geolud-client/projects/geolud-client/app/core/environment.initializer.ts
@@ -1,11 +1,13 @@
 import { APP_INITIALIZER, Provider } from '@angular/core';
 import { AppConfigService } from '../app-config.service';
 
+export function loadConfig(appConfigService: AppConfigService) {
+  return () => appConfigService.loadConfig();
+}
+
 export const EnvironmentInitializerProvider: Provider = {
   provide: APP_INITIALIZER,
-  useFactory(appConfigService: AppConfigService) {
-    return () => appConfigService.loadConfig();
-  },
+  useFactory: loadConfig,
   deps: [AppConfigService],
   multi: true,
 };
--- geolud-client/projects/geolud-client/app/core/graphql.apollo.options.ts
+++ geolud-client/projects/geolud-client/app/core/graphql.apollo.options.ts
@@ -5,13 +5,15 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import { Provider } from '@angular/core';
 import { ApolloClientOptions } from 'apollo-client';
 
+export function apolloOptions(httpLink: HttpLink): ApolloClientOptions<any>  {
+  return {
+    link: httpLink.create({ uri: environment.appConfig.apiBase + '/graphql' }),
+    cache: new InMemoryCache(),
+  };
+}
+
 export const GraphqlApolloOptionsProvider: Provider = {
   provide: APOLLO_OPTIONS,
-  useFactory(httpLink: HttpLink): ApolloClientOptions<any> {
-    return {
-      link: httpLink.create({ uri: environment.appConfig.apiBase + '/graphql' }),
-      cache: new InMemoryCache(),
-    };
-  },
+  useFactory: apolloOptions,
   deps: [HttpLink],
 };
