3a5e00892e29d27271673294c0c32fcfc0e2079b
fischermatte <fischermatte@posteo.net>
2019-03-05 00:39:08 +0100

    fixed resolver

--- geolud-server-node/src/contact/contact.resolver.ts
+++ geolud-server-node/src/contact/contact.resolver.ts
@@ -11,7 +11,7 @@ export class ContactResolvers {
   constructor(@Inject('MailService') private mailService: MailService) {}
 
   @Mutation('submitContactRequest')
-  submitContactRequest(@Args('name') name: string, @Args('name') email: string, @Args('name') message: string): Delivery {
+  submitContactRequest(@Args('name') name: string, @Args('email') email: string, @Args('message') message: string): Delivery {
     this.mailService.sendEmail(name, email, 'GEOLUD-SITE: Contact Request', message);
     pubSub.publish('contactRequestSubmitted', { contactRequestSubmitted: 'contact request sent' });
     return {
