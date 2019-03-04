f317af8e1d67600173e78bcb1d3c3178f1f4b790
fischermatte <fischermatte@posteo.net>
2019-03-04 23:41:51 +0100

    tslint fix

--- geolud-server-node/src/contact/contact.resolver.ts
+++ geolud-server-node/src/contact/contact.resolver.ts
@@ -12,10 +12,8 @@ export class ContactResolvers {
 
   @Mutation('submitContactRequest')
   submitContactRequest(@Args('name') name: string, @Args('name') email: string, @Args('name') message: string): Delivery {
-    console.info(`receiving contact request from ${email}`);
     this.mailService.sendEmail(name, email, 'GEOLUD-SITE: Contact Request', message);
     pubSub.publish('contactRequestSubmitted', { contactRequestSubmitted: 'contact request sent' });
-    console.info(`email sent from ${email}`);
     return {
       sentAt: new Date(),
     };
@@ -26,7 +24,6 @@ export class ContactResolvers {
     return {
       subscribe: () => {
         pubSub.asyncIterator('contactRequestSubmitted');
-        console.log('contact request submitted');
       },
     };
   }
